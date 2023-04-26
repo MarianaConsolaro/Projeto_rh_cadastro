35c3d7ee5107d2245568b759676e05e036e75232
Gisele de Godoy Cruz <116804045+GiseleGCruz@users.noreply.github.com>
2023-04-25 23:48:17 -0300

    Alterações tela de ListagemFiltro

--- Taking-ProjetoRh-GitHub/src/components/ListagemFiltro.js
+++ Taking-ProjetoRh-GitHub/src/components/ListagemFiltro.js
@@ -3,138 +3,42 @@ import { Form, Label, Input, Row, Col } from "reactstrap";
 
 export default function ListagemFiltro(){
 
-    return(
-    
-    
-        <>
+return(
         
-            <Images/>
+    <>
+       
+    <Images/>
     
-                
-                <body>
-                    <div>
+    <body>
+    <div>
     
-                    <center>
-                    <h1>Consulta de Currículos</h1>
-                    </center>
-    
-                    <br/>
+        <h4>CONSULTA DE CURRÍCULOS</h4>
+   
+        <br/>
 
-                    <body>
-                
-                <h4>Preencha todos os campos abaixo:</h4>
-                <br />
+        <h5>Informe o CFP:</h5>
+
+        
+        <body>
                
-                <Form>    
-                <Row>
-                <Col>
-                        <div className="form-group row">
-                            <Label for="Nome" className="col-sm-2.5 col-form-label">Nome </Label>
-                            <div className="col-sm-12">
-                            <Input type="text" className="form-control" id="" placeholder="Digite o primeiro nome"/>
-                            </div>
-                        </div>
-                </Col>
-
-                <br/>
-                <Col>
-                         <div className="form-group row">
-                            <Label for="Sobrenome" className="col-sm-2.5 col-form-label">Sobrenome </Label>
-                             <div className="col-sm-12">
-                            <Input type="text" className="form-control" id="" placeholder="Digite o sobrenome"/>
-                            </div>
-                        </div>
-                </Col>
-                </Row>
-
-                <br/>
-
-                <Row>
-                <Col>  
-                        <div className="form-group row">
-                             <Label for="CPF" className="col-sm-2.5 col-form-label">CPF</Label>
-                            <div className="col-sm-12">
-                             <Input type="text" className="form-control" id="" placeholder="Digite o CPF"/>
-                            </div>
-                        </div>
-                </Col>
-
-                <Col>
-                        <div className="form-group row">
-                             <Label for="Email" className="col-sm-2.5 col-form-label">E-mail</Label>
-                             <div className="col-sm-12">
-                             <Input type="text" className="form-control" id="" placeholder="Digite o e-mail"/>
-                            </div>
-                        </div>
-
-                </Col>  
-                </Row>
-
-                 <br/>
-
-            <Row>
-            <Col>
-                  
-                  <Label className="col-sm-2.5 col-form-label" for="pais">País</Label>
-                  <div className="col-sm-12">
-                  <select className="custom-select-listagem localizacao-opcao col-form-label" name="pais">
-                  <option value="todos">País</option>
-                  <option value="brasil">Brasil</option>
-                  <option value="USA">USA </option>
-                  <option value="Canada">Canada </option>
-                  </select>
-                  </div>
-                  
-                  
-            </Col> 
-            <Col>
-
-                <Label className="col-sm-2.5 col-form-label" for="estado">UF</Label>
-                    <div className="col-sm-12">
-                    <select className="custom-select-listagem localizacao-opcao col-form-label" name="estado">
-                    <option value="todos">UF</option>
-                    <option value="SP">SP </option>
-                    <option value="RJ">RJ </option>
-                    <option value="MG">MG </option>
-                    </select>
-                    </div>
-                        
-            </Col>
-            <Col>  
-
-                    <Label className="col-sm-2.5 col-form-label" for="cidade">Cidade</Label>
-                    <div className="col-sm-12">
-                    <select className="custom-select-listagem localizacao-opcao col-form-label" name="cidade">
-                    <option value="todos">Cidade</option>
-                    <option value="Sao Paulo">São Paulo </option>
-                    <option value="Rio de Janeiro">Rio de Janeiro </option>
-                    <option value="Belo horizonte">Belo horizonte </option>
-                    </select>
-                    </div>
-                   
-            </Col>
-            </Row>
-
-            <br />
-            
-            <br />
-                <br />
-
-                    <Row>                    
-                    <Col>                  
-                    <input class="button-buscar" type="button" value="Buscar"></input> 
-                    </Col>
-                    </Row>
-                 </Form>  
-                </body>
-
+        <Form>    
+       
+            <div className="form-group row">
+            <Label for="CPF" className="col-sm-2.5 col-form-label">CPF</Label>
+            <div className="col-sm-12">
+            <Input type="text" className="form-control" id="" placeholder="Digite o número do documento"/>
+            </div>
+            </div>
                 
-                    </div>
-                    
-                    
+        <br />
+            <input class="button-buscar" type="button" value="Buscar"></input> 
+        </Form>  
+    
+        </body>
 
-                </body>
+    </div>         
+    </body>
 
-                </>
+    </>
 
 )}
