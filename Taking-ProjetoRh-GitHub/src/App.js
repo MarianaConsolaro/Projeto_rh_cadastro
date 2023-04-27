65ff3035bc61b2f64ba040f3365fd976b5958391
Bruna Ariele <bruna.arisoares@gmail.com>
2023-04-27 17:39:05 -0300

    Criação da home page

--- Taking-ProjetoRh-GitHub/src/App.js
+++ Taking-ProjetoRh-GitHub/src/App.js
@@ -2,18 +2,27 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Cadastro from './components/Cadastro';
 import ListagemFiltro from './components/ListagemFiltro';
+import Home from './components/Home'
+
+
+
+
 
 
 
 function App() {
   return (
-    <div className="App">          
-    
+    <div className="App">  
+  
 
       <Routes>
-
-        <Route path = "/" element = {<Cadastro/>} /> 
+      <Route path = "/Home" element = {<Home/>} /> 
+        <Route path = "/Cadastro" element = {<Cadastro/>} /> 
         <Route path = "/listagemfiltro" element = {<ListagemFiltro/>} /> 
+        
+        
+       
+
 
       </Routes>
 
---
+++ Taking-ProjetoRh-GitHub/src/components/Home.js
@@ -0,0 +1,28 @@
+import { Link, NavLink } from 'react-router-dom';
+import { Nav, Navbar, NavbarBrand, NavItem } from 'reactstrap';
+import Images from './images';
+
+
+export default function Home(){
+
+
+    return( 
+        <>
+
+        <Navbar  color="navbar navbar-dark bg-dark">
+            <NavbarBrand tag={Link} to="/Home">Home</NavbarBrand>
+            <NavbarBrand tag={Link} to="/Cadastro">Cadastro de currículo</NavbarBrand>
+            <NavbarBrand tag={Link} to="/RH">RH</NavbarBrand>
+            <Nav className="me-auto" navbar>
+                
+            </Nav>
+            
+        </Navbar>
+        
+            <div>
+                <img className='TK' src="/LogoTK.png" alt='Imgem da taking'/>
+            </div>
+        </>
+        
+    );
+}
